ab65f2e01453f5898e8ee9938f492f670c6d2ce8
Nataly Baranova <borashek@inbox.ru>
2024-07-20 11:48:37 +0300

    feat: format prices of details from favorit api

--- src/features/autoparts/Autoparts.tsx
+++ src/features/autoparts/Autoparts.tsx
@@ -35,6 +35,7 @@ export const Autoparts = () => {
           placeholder='Введите VIN детали, название или модель авто'
           setNumber={searchAutoparts} 
         />
+        
       </div>
       {isLoading && <Loader />}
       {error && <Error error={error} />}
@@ -45,10 +46,12 @@ export const Autoparts = () => {
               return (
                 <div key={autopart.goodsID} className="w-80 p-6 bg-gray-100 hover:bg-gray- border border-gray-200 rounded-lg shadow">
                   <Link href="#">
-                    <h5 className="text-xl tracking-tight text-black">{autopart.name.slice(0, 40)}...</h5>
+                    <h5 className="text-xl tracking-tight text-black">
+                      {autopart.name.slice(0, 40)}...
+                    </h5>
                     <p className="mb-4">подробнее...</p>
                   </Link>
-                  <p className="mb-3 font-normal text-black">{autopart.price * 1.2} руб.</p>
+                  <p className="mb-3 font-normal text-black">{(autopart.price * 1.2).toFixed(2)} руб.</p>
                   <Link href={'#'} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                       В корзину
                     <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
