6fb9c89277ffe9ef74b5fbb6c293f0846151b49d
Nataly Baranova <borashek@inbox.ru>
2024-07-23 11:11:29 +0300

    feat: change support button

--- src/shared/components/call-support/CallSupport.tsx
+++ src/shared/components/call-support/CallSupport.tsx
@@ -1,10 +1,8 @@
 'use client'
 
-import Image from 'next/image';
-import { Modal } from '../modal/Modal';
 import styles from './CallSupport.module.css';
-import support from '@/shared/assets/icons/support.svg';
 import { useState } from "react";
+import { SupportLink } from '../links/SupportLink';
 
 export const CallSupport = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,22 +13,14 @@ export const CallSupport = () => {
         onClick={() => setIsOpen(!isOpen)}
         className={styles.support__imgWrapper}
       >
-        <Image 
-          src={support} 
-          alt="Позвоните в тех. поддержку" 
-          className={styles.support__img}  
-        />
+        <SupportLink phone='+79169174630' />
       </div>
-      {isOpen && 
-        <div className={styles.support__modal}>
-          <Modal 
-            title='Техническая поддержка'
-            desc='Оставьте вашу заявку на консультацию с менеджером и мы свяжемся с вами в течение 10 мнут'
-            phone='+79169174630'
-            email='borashek@inbox.ru'
-          />
-        </div>
-      }
+      <div 
+        onClick={() => setIsOpen(!isOpen)}
+        className={styles.support__imgWrapper}
+      >
+        <SupportLink email='info@global-treid-m.com' />
+      </div>
     </div>
   )
 }
--- src/shared/components/modal/Modal.tsx
+++ src/shared/components/modal/Modal.tsx
@@ -1,9 +1,4 @@
-import Image from "next/image";
 import { Button } from "../button/Button";
-import emailImg from '@/shared/assets/icons/email.svg';
-import phoneImg from '@/shared/assets/icons/phone.svg';
-import Link from "next/link";
-import { AppLink } from "../links/SupportLink";
 
 type Props = {
   title?: string,
@@ -24,10 +19,6 @@ export const Modal = ({
   return (
     <div className="w-64 p-6 bg-white border border-gray-200 shadow dark:bg-gray-800 dark:border-gray-700">
       <p className="mb-3 font-normal text-center text-gray-900">{desc}</p>
-      <div className="m-4">
-        <AppLink phone={phone} />
-        <AppLink email={email} />
-      </div>
       {buttonName && <Button name={buttonName} type='button' />}
     </div>
   )
