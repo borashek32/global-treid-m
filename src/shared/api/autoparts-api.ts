a9c8b34cdd7f2a8326b992ce8ddf5c57da479840
Nataly Baranova <borashek@inbox.ru>
2024-08-13 15:05:22 +0300

    fix: fix autoparts fetch after search

--- src/shared/api/autoparts-api.ts
+++ src/shared/api/autoparts-api.ts
@@ -3,6 +3,7 @@ import { instance } from "./instanse";
 
 export const autopartsApi = {
   getAutoparts(number: string) {
+    console.log(number)
     return instance.get<{goods: ProductFromFavoritApiType[]}>('/', {
       params: {
         key: '30D2E199-5AAE-4837-AABD-74518EE085FD',
